62927c6b94328a1ffe91c5febf4b78b9d9996671
dagmawig <dgebreselasse@gmail.com>
2023-03-14 21:30:32 -0500

    made search box responsive to exercise name input

--- components/ExerComp.js
+++ components/ExerComp.js
@@ -2,25 +2,26 @@ import { View, Text, Image } from 'react-native'
 import React, { useRef } from 'react'
 import { IMAGES } from '../assets'
 import exerLocal from '../assets/ExerData/exercisesLocal.json';
-const ExerComp = (filter) => {
+import exerNames from '../assets/ExerData/exerciseNames.json';
 
-    return exerLocal.map((exer, i) => {
+const ExerComp = ({filterExer}) => {
+    return filterExer.map((exer, i) => {
         return (
             <View key={`exer-${i}`}>
                 {/* <Text className='text-gray-400'>{exer.name}</Text> */}
                 <View className='flex-row mb-2 space-x-2 bg-[#28547B] border-red-500'>
                     <View>
                         <Image
-                            source={IMAGES[i]}
+                            source={IMAGES[exer.refIndex]}
                             className='w-20 h-20 object-cover'
                         />
                     </View>
                     <View className='w-64 justify-center'>
                         <Text className='text-lg text-white flex-wrap'>
-                            {exer.name}
+                            {exer.item.name}
                         </Text>
                         <Text className='text-white italic'>
-                            {exer.bodyPart}
+                            {exer.item.bodyPart}
                         </Text>
                     </View>
                 </View>
--- screens/Exercises.js
+++ screens/Exercises.js
@@ -3,11 +3,28 @@ import React, { useLayoutEffect, useState } from 'react'
 import { useNavigation } from '@react-navigation/native';
 import { FontAwesome5 } from '@expo/vector-icons';
 import ExerComp from '../components/ExerComp';
+import exerLocal from '../assets/ExerData/exercisesLocal.json';
+import exerNames from '../assets/ExerData/exerciseNames.json';
+import Fuse from 'fuse.js';
 
 const Exercises = () => {
 
+    const options = {
+        includeScore: true,
+        shouldSort: true,
+        findAllMatches: true,
+        useExtendedSearch: true,
+        ignoreLocation: true,
+        threshold: 0.06,
+        keys: ['name']
+    }
+
+    const exerFuse = new Fuse(exerLocal, options);
+
     const navigation = useNavigation();
     const [searchMode, updateSearchMode] = useState(true);
+    const [exerArr, setExerArr] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
 
     useLayoutEffect(() => {
         navigation.setOptions({
@@ -15,15 +32,25 @@ const Exercises = () => {
         })
     }, [])
 
+    function handleChange(e) {
+        if(!e.target.value.split(' ').join('')) setExerArr(exerFuse.search("!0123456789"))
+        else setExerArr(exerFuse.search("'" + e.target.value.trim()))
+        console.log(exerArr)
+    }
+
+    function handleBack() {
+        updateSearchMode(false);
+    }
+
     return (
         <View className='bg-[#28547B] flex-1 max-h-screen min-w-screen overflow-hidden'>
             <View className='pt-[20px] h-full w-full' >
                 <View className='w-full h-10 shadow-2xl flex-row items-center justify-between px-3 sticky'>
                     {searchMode ? <>
-                        <TouchableOpacity onPress={e => updateSearchMode(false)}>
+                        <TouchableOpacity onPress={handleBack}>
                             <FontAwesome5 name="arrow-left" size={17} color="white" />
                         </TouchableOpacity>
-                        <TextInput className='h-7 w-60  border-white border-2 rounded-md bg-[#345b7c] px-2 text-white' cursorColor={'white'} placeholder='exercise name' placeholderTextColor={'gray'} />
+                        <TextInput className='h-7 w-60  border-white border-2 rounded-md bg-[#345b7c] px-2 text-white' cursorColor={'white'} placeholder='exercise name' placeholderTextColor={'gray'} onChange={(e)=>handleChange(e)} />
                         <TouchableOpacity className=''>
                             <FontAwesome5 name="filter" size={16} color="white" />
                         </TouchableOpacity>
@@ -57,8 +84,11 @@ const Exercises = () => {
                     </View> */}
                 </View>
                 <ScrollView className='mb-11 px-3'>
-
-                    {/* <ExerComp /> */}
+                    {/* <View className='items-center space-x-2 rounded-xl border-2 border-white'>
+                        <Text className='text-white'>cable</Text>
+                        <TouchableOpacity><FontAwesome5 name="times" size={16} color="white" /></TouchableOpacity>
+                    </View> */}
+                    <ExerComp filterExer={exerArr} />
                 </ScrollView>
                 <View className='absolute bottom-0 h-10 bg-[#28547B] w-full items-center justify-center'>
                     <Text> dfgdf</Text>
